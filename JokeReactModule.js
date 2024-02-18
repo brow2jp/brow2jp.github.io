525f820a2f3315b66fb361d3cbe2ce36409948d4
John Brown <97489979+jpbrown46@users.noreply.github.com>
2024-02-18 15:42:40 -0500

    More changes.

--- JokeReactModule.js
+++ JokeReactModule.js
@@ -9,8 +9,6 @@ class JokeTimer extends React.Component {
     async fetchJoke () {
         const jokeResponse = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single');
         const jokeResult = await jokeResponse.json();
-        console.log(jokeResponse);
-        console.log(jokeResult)
         this.setState({ currentJoke: jokeResult.joke });
     }
 
